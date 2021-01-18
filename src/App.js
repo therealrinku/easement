817fd154b782f5a926fae3fb4b173182f97be4d4
rinkuuu <backupfb2032@gmail.com>
2021-01-18 16:23:44 +0545

    students page init

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Topbar from "./components/Topbar/Topbar";
 import "./App.css";
+import Students from "./pages/Students/Students";
 
 const App = () => {
   const [showSidebar, setShowSidebar] = useState(true);
@@ -15,6 +16,13 @@ const App = () => {
           toggleSidebar={() => setShowSidebar((prev) => !prev)}
           sideBarIsClosed={!showSidebar}
         />
+
+        <div
+          className="main"
+          style={showSidebar ? { marginLeft: "17.5%" } : null}
+        >
+          <Route path="/students" exact component={Students} />
+        </div>
       </BrowserRouter>
     </div>
   );
--- src/pages/Students/Students.js
+++ src/pages/Students/Students.js
@@ -1,7 +1,59 @@
-import React from "react";
+import { Tooltip } from "@material-ui/core";
+import { MdEdit } from "react-icons/all";
+import "./Students.css";
 
 const Students = () => {
-  return <div className="students"></div>;
+  const sortedStudents = [
+    { id: 1, name: "rinku", class: 1, rollNo: 10 },
+    { id: 1, name: "olivia", class: 1, rollNo: 11 },
+  ];
+
+  return (
+    <div className="students--page">
+      <h4>Students</h4>
+      <table className="table">
+        <thead>
+          <th>S.N~{sortedStudents.length}</th>
+          <th>Name</th>
+          <th>Class</th>
+          <th>RollNo</th>
+        </thead>
+        <tbody>
+          {sortedStudents.map((student, i) => {
+            return (
+              <tr key={student.id}>
+                <td>{i + 1}</td>
+                <td
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    border: "none",
+                  }}
+                >
+                  <p>{student.name}</p>
+                  <div>
+                    <Tooltip title={`Edit ${student.name}`}>
+                      <button
+                        style={{
+                          border: "none",
+                          background: "none",
+                          outline: "none",
+                        }}
+                      >
+                        <MdEdit />
+                      </button>
+                    </Tooltip>
+                  </div>
+                </td>
+                <td>{student.class}</td>
+                <td>{student.rollNo}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Students;
