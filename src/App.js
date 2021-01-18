4e80177c23c6b400957e586168a022689fbd6350
rinkuuu <backupfb2032@gmail.com>
2021-01-18 14:09:10 +0545

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
-  return <div className="app"></div>;
+  return (
+    <BrowserRouter>
+      <Route path="/" component={Sidebar} />
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,69 @@
+import "./Sidebar.css";
+
+import {
+  HiOutlineSearch,
+  MdAdd,
+  FiUsers,
+  MdAttachMoney,
+  MdShowChart,
+  BsGear,
+  VscHome,
+  AiOutlineDelete,
+  RiAdminLine,
+} from "react-icons/all";
+import { NavLink } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h4>Easement</h4>
+
+      <NavLink to="/" activeClassName="nav--active">
+        <VscHome />
+        <p>Home</p>
+      </NavLink>
+
+      <NavLink to="/search" activeClassName="nav--active">
+        <HiOutlineSearch />
+        <p>Quick Search</p>
+      </NavLink>
+
+      <NavLink to="/new" activeClassName="nav--active">
+        <MdAdd />
+        <p>Add Student</p>
+      </NavLink>
+
+      <NavLink to="/staffs" activeClassName="nav--active">
+        <RiAdminLine />
+        <p>Staffs</p>
+      </NavLink>
+
+      <NavLink to="/students" activeClassName="nav--active">
+        <FiUsers />
+        <p>Students</p>
+      </NavLink>
+
+      <NavLink to="/fees" activeClassName="nav--active">
+        <MdAttachMoney />
+        <p>Fees</p>
+      </NavLink>
+
+      <NavLink to="/results" activeClassName="nav--active">
+        <MdShowChart />
+        <p>Results</p>
+      </NavLink>
+
+      <NavLink to="/trash" activeClassName="nav--active">
+        <AiOutlineDelete />
+        <p>Trash</p>
+      </NavLink>
+
+      <NavLink to="/settings" activeClassName="nav--active">
+        <BsGear />
+        <p>Settings</p>
+      </NavLink>
+    </div>
+  );
+};
+
+export default Sidebar;
