c6d363e398312d5177e066d75d45719abc24e837
rinkuuu <backupfb2032@gmail.com>
2021-01-20 07:55:52 +0545

    state added to forms

--- src/components/AddStaffForm.js
+++ src/components/AddStaffForm.js
@@ -1,14 +1,35 @@
+import { useState } from "react";
+
 const AddStaffForm = () => {
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
+  const [salary, setSalary] = useState("");
+
   return (
     <form className="new--form">
       <label htmlFor="name">Staff Name</label>
-      <input type="text" id="name" />
+      <input
+        type="text"
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label htmlFor="role">Role</label>
-      <input type="text" id="role" />
+      <input
+        type="text"
+        id="role"
+        value={role}
+        onChange={(e) => setRole(e.target.value)}
+      />
 
       <label htmlFor="salary">Salary</label>
-      <input type="text" id="salary" />
+      <input
+        type="number"
+        id="salary"
+        value={salary}
+        onChange={(e) => setSalary(e.target.value)}
+      />
 
       <button>Submit</button>
     </form>
--- src/components/AddStudentForm.js
+++ src/components/AddStudentForm.js
@@ -1,14 +1,35 @@
+import { useState } from "react";
+
 const AddStudentForm = () => {
+  const [name, setName] = useState("");
+  const [CLASS, setClass] = useState("");
+  const [rollNo, setRollNo] = useState("");
+
   return (
     <form className="new--form">
       <label htmlFor="name">Student Name</label>
-      <input type="text" id="name" />
+      <input
+        type="text"
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label htmlFor="class">Class</label>
-      <input type="text" id="class" />
+      <input
+        type="text"
+        id="class"
+        value={CLASS}
+        onChange={(e) => setClass(e.target.value)}
+      />
 
-      <label htmlFor="rollno">Rollno</label>
-      <input type="text" id="rollno" />
+      <label htmlFor="number">Rollno</label>
+      <input
+        type="text"
+        id="rollno"
+        value={rollNo}
+        onChange={(e) => setRollNo(e.target.value)}
+      />
 
       <button>Submit</button>
     </form>
