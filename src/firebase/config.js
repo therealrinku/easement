1494fb7ebe01cd02019a0bcb4d44393ce8c70d5a
rinkuuu <backupfb2032@gmail.com>
2021-01-20 08:17:40 +0545

    firebase setup done

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBSH1gmI5iVur72ku8S359QcHG56NlEJNM",
+  authDomain: "easeement.firebaseapp.com",
+  projectId: "easeement",
+  storageBucket: "easeement.appspot.com",
+  messagingSenderId: "738557183883",
+  appId: "1:738557183883:web:c904a2fe02ac3eb91a918e",
+  measurementId: "G-NLWKZTHFB4",
+};
+
+export default firebaseConfig;
