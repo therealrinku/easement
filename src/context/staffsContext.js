cfbb33619d142ca57d745e3fc1c12f6369bfbcf2
rinkuuu <backupfb2032@gmail.com>
2021-01-21 17:27:47 +0545

    contexts added

---
+++ src/context/staffsContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+const staffsContext = createContext({
+  staffs: [],
+});
+
+export default staffsContext;
---
+++ src/context/studentsContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+const studentsContext = createContext({
+  students: [],
+});
+
+export default studentsContext;
