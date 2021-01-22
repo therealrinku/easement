70484a21c8cfea89119bd96f92eeeb10767cba47
rinkuuu <backupfb2032@gmail.com>
2021-01-22 20:36:36 +0545

    problem fix

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,15 +3,8 @@ import { MdDone, VscError } from "react-icons/all";
 const Alert = ({ msg }) => {
   return (
     <div className="alert">
-      <section style={msg.includes("Succesfully") ? { display: "none" } : null}>
-        <MdDone />
-        <p>Successfully done!</p>
-      </section>
-
-      <section
-        style={!msg.includes("Succesfully") ? { display: "none" } : null}
-      >
-        <VscError />
+      <section>
+        {msg.includes("created") ? <MdDone /> : <VscError />}
         <p>{msg}</p>
       </section>
     </div>
