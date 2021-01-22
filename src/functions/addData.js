33b04d43e3adb4266950d5af1e02de39446aaa66
rinkuuu <backupfb2032@gmail.com>
2021-01-22 20:26:34 +0545

    add data function changed to promise

--- src/functions/addData.js
+++ src/functions/addData.js
@@ -2,11 +2,15 @@ import db from "../firebase/db";
 import firebase from "firebase/app";
 
 const addPerson = (currentUser, personType, personData) => {
-  db.collection(currentUser)
-    .doc(personType)
-    .update({
-      [personType]: firebase.firestore.FieldValue.arrayUnion(personData),
-    });
+  return new Promise((resolve) => {
+    db.collection(currentUser)
+      .doc(personType)
+      .update({
+        [personType]: firebase.firestore.FieldValue.arrayUnion(personData),
+      })
+      .then(() => resolve("done"))
+      .catch((err) => resolve(err.message));
+  });
 };
 
 export default addPerson;
