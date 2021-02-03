b7d2fa574ac0304870415144fe6c7939f3bcd9cd
rinkuuu <backupfb2032@gmail.com>
2021-02-03 15:09:25 +0545

    set data function added

---
+++ src/functions/setData.js
@@ -0,0 +1,12 @@
+import db from "../firebase/db";
+
+const setData = async (username, dataType, updatedData) => {
+  return db
+    .collection(username)
+    .doc(dataType)
+    .set({
+      [dataType]: updatedData,
+    });
+};
+
+export default getData;
