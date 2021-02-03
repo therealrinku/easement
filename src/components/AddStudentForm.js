1d8d0b6ac172b2c68fffb35dbf102671d7fd82d8
rinkuuu <backupfb2032@gmail.com>
2021-02-03 07:37:33 +0545

    students form validation added

--- src/components/AddStudentForm.js
+++ src/components/AddStudentForm.js
@@ -1,27 +1,42 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import addPerson from "../functions/addData";
 import Alert from "./Alert";
+import Context from "../context/Context";
 
 const AddStudentForm = () => {
   const [name, setName] = useState("");
   const [CLASS, setClass] = useState("");
   const [rollNo, setRollNo] = useState("");
   const [msg, setMsg] = useState("");
+  const { students } = useContext(Context);
 
   const AddStudent = (e) => {
     e.preventDefault();
-    addPerson("test", "students", { name, class: CLASS, rollNo }).then(
-      (res) => {
-        if (res === "done") {
-          setMsg("Succesfully created a new student.");
-          setName("");
-          setClass("");
-          setRollNo("");
+
+    if (name.trim().length >= 3 && name.trim().length <= 60) {
+      if (CLASS !== "") {
+        if (rollNo > 0) {
+          addPerson("test", "students", { name, class: CLASS, rollNo }).then(
+            (res) => {
+              if (res === "done") {
+                setMsg("Succesfully created a new student.");
+                setName("");
+                setClass("");
+                setRollNo("");
+              } else {
+                setMsg(res);
+              }
+            }
+          );
         } else {
-          setMsg(res);
+          setMsg("Rollno cannot be empty or below 1.");
         }
+      } else {
+        setMsg("Class cannot be empty.");
       }
-    );
+    } else {
+      setMsg("Name must be between 3 and 60 charcters.");
+    }
 
     setTimeout(() => {
       setMsg("");
