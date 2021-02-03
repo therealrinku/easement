59795c634d668b3c3d3b437ad91e39543b9b785a
rinkuuu <backupfb2032@gmail.com>
2021-02-03 13:53:32 +0545

    backdrop added

---
+++ src/components/Backdrop.js
@@ -0,0 +1,20 @@
+const backdropStyles = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100vh",
+  background: "rgba(0,0,0,0.7)",
+  zIndex: "2",
+};
+
+const Backdrop = ({ show, toggle }) => {
+  return (
+    <div
+      style={show ? backdropStyles : { display: "none" }}
+      onClick={toggle}
+    ></div>
+  );
+};
+
+export default Backdrop;
