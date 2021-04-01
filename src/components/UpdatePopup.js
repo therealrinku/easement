94e5dfc7ee8fb0730817878d89ae98badee20b3c
therealrinku <backupfb2032@gmail.com>
2021-04-01 14:18:33 +0545

    update popup created

---
+++ src/components/UpdatePopup.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "../styles/UpdatePopup.css";
+
+const UpdatePopup = ({ personName, propertyName, propertyValue }) => {
+  const [val, setVal] = useState(propertyValue);
+  return (
+    <div className="update--popup">
+      <h4>Edit {personName}</h4>
+      <form>
+        <label htmlFor="property-value">{propertyName}</label>
+        <input
+          type="text"
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+        <button type="submit">Update</button>
+        <button type="button">Cancel</button>
+      </form>
+    </div>
+  );
+};
+
+export default UpdatePopup;
