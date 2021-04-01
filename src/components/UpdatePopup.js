e07a215ca88c67792e0f73688f55659d06705434
therealrinku <backupfb2032@gmail.com>
2021-04-01 14:27:12 +0545

    label reomved

--- src/components/UpdatePopup.js
+++ src/components/UpdatePopup.js
@@ -1,18 +1,18 @@
 import { useState } from "react";
 import "../styles/UpdatePopup.css";
 
-const UpdatePopup = ({ personName, propertyName, propertyValue, toggle }) => {
+const UpdatePopup = ({ propertyName, propertyValue, toggle }) => {
   const [val, setVal] = useState(propertyValue);
   return (
     <div className="update--popup">
-      <h4>Edit {personName}</h4>
+      <h4>Edit {propertyName}</h4>
       <form>
-        <label htmlFor="property-value">{propertyName}</label>
         <input
           type="text"
           value={val}
           onChange={(e) => setVal(e.target.value)}
         />
+        <br />
         <button type="submit">Update</button>
         <button type="button" onClick={toggle}>
           Cancel
