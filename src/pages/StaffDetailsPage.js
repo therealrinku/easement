b51e37edb9369d3f0906b206c031e60965940b2c
therealrinku <backupfb2032@gmail.com>
2021-04-02 14:05:30 +0545

    showing loader done

--- src/pages/StaffDetailsPage.js
+++ src/pages/StaffDetailsPage.js
@@ -6,11 +6,13 @@ import Backdrop from "../components/Backdrop";
 import overflowToggler from "../utils/OverflowToggler";
 import Detail from "../components/Detail";
 import db from "../firebase/db";
+import Loader from "../components/Loader";
 
 const StaffDetailsPage = () => {
   const [details, setDetails] = useState([]);
   const [showDeletePopup, setShowDeletePopup] = useState(false);
   const [showEditButtons, setShowEditButtons] = useState(false);
+  const [loading, setLoading] = useState(true);
   const params = useParams();
   const history = useHistory();
 
@@ -27,6 +29,7 @@ const StaffDetailsPage = () => {
           }
         }
         setDetails(filteredData);
+        setLoading(false);
       });
   }, [params.studentId]);
 
@@ -45,37 +48,55 @@ const StaffDetailsPage = () => {
 
   return (
     <Fragment>
-      <h4>Staff Details</h4>
-      <img src="https://bit.ly/3m1Ny2x" className="avatar--image" />
-      {/*dynamic data mapping*/}
-      {details.map((detail, i) => {
-        return (
-          <Detail
-            propertyName={detail[0]}
-            propertyValue={detail[1]}
-            key={i}
-            showEditButtons={showEditButtons}
-            personId={params.staffId}
-          />
-        );
-      })}
-      <section className="control-buttons">
-        <button onClick={() => setShowEditButtons((prev) => !prev)}>
-          Edit
-        </button>
-        <button onClick={() => toggleModal(setShowDeletePopup)}>Delete</button>
-      </section>
-
-      {showDeletePopup ? (
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "400px",
+          }}
+        >
+          <Loader />
+        </div>
+      ) : (
         <Fragment>
-          <DeleteConfirmPopup
-            toggle={() => toggleModal(setShowDeletePopup)}
-            name={details.staffName}
-            Delete={deleteStaffConfirm}
-          />
-          <Backdrop toggle={() => toggleModal(setShowDeletePopup)} />
+          <h4>Staff Details</h4>
+          <img src="https://bit.ly/3m1Ny2x" className="avatar--image" />
+          {/*dynamic data mapping*/}
+          {details.map((detail, i) => {
+            return (
+              <Detail
+                propertyName={detail[0]}
+                propertyValue={detail[1]}
+                key={i}
+                showEditButtons={showEditButtons}
+                personId={params.staffId}
+              />
+            );
+          })}
+          <section className="control-buttons">
+            <button onClick={() => setShowEditButtons((prev) => !prev)}>
+              Edit
+            </button>
+            <button onClick={() => toggleModal(setShowDeletePopup)}>
+              Delete
+            </button>
+          </section>
+
+          {showDeletePopup ? (
+            <Fragment>
+              <DeleteConfirmPopup
+                toggle={() => toggleModal(setShowDeletePopup)}
+                name={details.staffName}
+                Delete={deleteStaffConfirm}
+              />
+              <Backdrop toggle={() => toggleModal(setShowDeletePopup)} />
+            </Fragment>
+          ) : null}
         </Fragment>
-      ) : null}
+      )}
     </Fragment>
   );
 };
