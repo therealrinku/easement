0095d921389a155d2f755734b4d172fdda64e37b
therealrinku <backupfb2032@gmail.com>
2021-04-03 07:28:12 +0545

    show no any students added message if no student is there

--- src/pages/StudentsPage.js
+++ src/pages/StudentsPage.js
@@ -15,42 +15,63 @@ const StudentsPage = () => {
 
   return (
     <Fragment>
-      <h4>Students</h4>
-      <Filters searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      {students.length > 0 ? (
+        <Fragment>
+          <h4>Students</h4>
+          <Filters searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
 
-      <table className="table">
-        <thead>
-          <tr>
-            <th>S.N~{filteredStudents.length}</th>
-            <th>Name</th>
-            <th>Class</th>
-            <th>RollNo</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {filteredStudents.map((student, i) => {
-            return (
-              <tr key={i}>
-                <td>{i + 1}</td>
-                <td
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    border: "none",
-                  }}
-                >
-                  <Link to={`/student/details/${student.id}`} className="link">
-                    {student.Name}
-                  </Link>
-                </td>
-                <td>{student.Class}</td>
-                <td>{student.RollNo}</td>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>S.N~{filteredStudents.length}</th>
+                <th>Name</th>
+                <th>Class</th>
+                <th>RollNo</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+
+            <tbody>
+              {filteredStudents.map((student, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{i + 1}</td>
+                    <td
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        border: "none",
+                      }}
+                    >
+                      <Link
+                        to={`/student/details/${student.id}`}
+                        className="link"
+                      >
+                        {student.Name}
+                      </Link>
+                    </td>
+                    <td>{student.Class}</td>
+                    <td>{student.RollNo}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </Fragment>
+      ) : (
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <h4>No any students created yet!</h4>
+          <Link
+            to="/new"
+            style={{
+              color: "blue",
+              textDecoration: "none",
+              marginLeft: "5px",
+            }}
+          >
+            Create Now
+          </Link>
+        </div>
+      )}
     </Fragment>
   );
 };
