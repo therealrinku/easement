fb27dffdca4ca21bf180beab4bb162c120e4dd5c
therealrinku <backupfb2032@gmail.com>
2021-04-04 07:38:14 +0545

    class list view done

--- src/pages/ClassesPage.js
+++ src/pages/ClassesPage.js
@@ -1,9 +1,30 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import db from "../firebase/db";
 
 const ClassesPage = () => {
+  const [classes, setClasses] = useState([]);
+
+  useEffect(() => {
+    db.collection("classes")
+      .where("linkedUsername", "==", "test")
+      .onSnapshot((snap) => {
+        const data = [];
+        snap.docs.map((doc) => {
+          data.push({ ...doc.data(), id: doc.id });
+        });
+        setClasses(data);
+      });
+  }, []);
+
   return (
     <Fragment>
       <h4>Classes List</h4>
+      <div className="classes">
+        {classes.map((cl) => {
+          return <Link to={`/class/${cl.id}`}>{cl.className}</Link>;
+        })}
+      </div>
     </Fragment>
   );
 };
