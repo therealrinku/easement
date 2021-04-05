04f520caa3f9a1800143b0333098bfad66359f4b
therealrinku <backupfb2032@gmail.com>
2021-04-05 08:13:29 +0545

    no any classs msg added

--- src/components/ClassesDropdown.js
+++ src/components/ClassesDropdown.js
@@ -1,19 +1,44 @@
 import "../styles/ClassesDropdown.css";
+import { Link } from "react-router-dom";
 
 const ClassesDropdown = ({ classes, updateClassName }) => {
   return (
     <section className="classes--dropdown">
-      {classes.map((c, i) => {
-        return (
-          <button
-            key={i}
-            onClick={() => updateClassName(c.className)}
-            type="button"
+      {classes.length > 0 ? (
+        classes.map((c, i) => {
+          return (
+            <button
+              key={i}
+              onClick={() => updateClassName(c.className)}
+              type="button"
+            >
+              {c.className}
+            </button>
+          );
+        })
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontSize: "12px",
+            borderTop: "solid 1px rgba(0,0,0,0.2)",
+          }}
+        >
+          <h4>No any class added yet!</h4>
+          <Link
+            to="/new?class=true"
+            style={{
+              color: "blue",
+              textDecoration: "none",
+              marginLeft: "5px",
+            }}
           >
-            {c.className}
-          </button>
-        );
-      })}
+            Create Now
+          </Link>
+        </div>
+      )}
     </section>
   );
 };
