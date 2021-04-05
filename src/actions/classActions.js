4184f7be1bc78e5af891050a71cc49a6fba2e392
therealrinku <backupfb2032@gmail.com>
2021-04-05 07:31:44 +0545

    delete class func tion created

--- src/actions/classActions.js
+++ src/actions/classActions.js
@@ -10,3 +10,33 @@ export const addClass = (data) => {
       .catch((err) => resolve(err.message));
   });
 };
+
+export const deleteClass = (className) => {
+  return new Promise((resolve) => {
+    //deleteing class
+    db.collection("classes")
+      .where("className", "==", className)
+      .get()
+      .then((snap) => {
+        snap.forEach((doc) => doc.ref.delete());
+      })
+      .catch((err) => {
+        resolve(err.message);
+      })
+      .then(() => {
+        //deteling students of that class
+        db.collection("students")
+          .where("Class", "==", className)
+          .get()
+          .then((snap) => {
+            snap.forEach((doc) => doc.ref.delete());
+          })
+          .then(() => {
+            resolve(`Successfully deleted the ${className}`);
+          })
+          .catch((err) => {
+            resolve(err.message);
+          });
+      });
+  });
+};
