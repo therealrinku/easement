50552e5b961294280decf3355dcb017b5110fd18
therealrinku <backupfb2032@gmail.com>
2021-04-09 08:34:24 +0545

    layout compoenent created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { useEffect, useState, Fragment } from "react";
 import { Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import Topbar from "./components/Topbar";
 import StudentsPage from "./pages/StudentsPage";
 import FeesPage from "./pages/FeesPage";
 import ResultsPage from "./pages/ResultsPage";
@@ -11,71 +9,35 @@ import AddPage from "./pages/AddPage";
 import getData from "./actions/getData";
 import Context from "./context/Context";
 import StaffDetailsPage from "./pages/StaffDetailsPage";
-import Alert from "./components/Alert";
 import ClassesPage from "./pages/ClassesPage";
 import ClassDetailsPage from "./pages/ClassDetailsPage";
-import GoBack from "./components/GoBack";
+import Layout from "./components/Layout";
 import "./App.css";
 
 const App = () => {
-  const [showSidebar, setShowSidebar] = useState(true);
-  const [students, setStudents] = useState([]);
-  const [staffs, setStaffs] = useState([]);
   const [classes, setClasses] = useState([]);
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    getData("test", "students", setStudents);
-    getData("test", "staffs", setStaffs);
     getData("test", "classes", setClasses);
   }, []);
 
   return (
-    <div className="app">
-      {message ? <Alert msg={message} /> : null}
-      <Context.Provider
-        value={{
-          students,
-          staffs,
-          classes,
-          message,
-          setMessage,
-        }}
-      >
+    <Layout>
+      <Context.Provider value={{ classes, message, setMessage }}>
         <Fragment>
-          <Sidebar hideSidebar={!showSidebar} />
-          <Topbar
-            toggleSidebar={() => setShowSidebar((prev) => !prev)}
-            sideBarIsClosed={!showSidebar}
-          />
-          <div
-            className="page"
-            style={showSidebar ? { marginLeft: "17.5%" } : null}
-          >
-            <GoBack />
-            <Route path="/students" exact component={StudentsPage} />
-            <Route path="/fees" exact component={FeesPage} />
-            <Route path="/results" exact component={ResultsPage} />
-            <Route path="/staffs" exact component={StaffsPage} />
-            <Route path="/new" exact component={AddPage} />
-            <Route path="/classes" exact component={ClassesPage} />
-            <Route
-              path="/class/:className"
-              exact
-              component={ClassDetailsPage}
-            />
-            <Route
-              path="/student/details/:studentId"
-              component={StudentDetailsPage}
-            />
-            <Route
-              path="/staff/details/:staffId"
-              component={StaffDetailsPage}
-            />
-          </div>
+          <Route path="/students" exact component={StudentsPage} />
+          <Route path="/fees" exact component={FeesPage} />
+          <Route path="/results" exact component={ResultsPage} />
+          <Route path="/staffs" exact component={StaffsPage} />
+          <Route path="/new" exact component={AddPage} />
+          <Route path="/classes" exact component={ClassesPage} />
+          <Route path="/class/:className" exact component={ClassDetailsPage} />
+          <Route path="/student/details/:studentId" component={StudentDetailsPage} />
+          <Route path="/staff/details/:staffId" component={StaffDetailsPage} />
         </Fragment>
       </Context.Provider>
-    </div>
+    </Layout>
   );
 };
 
---
+++ src/components/Layout.js
@@ -0,0 +1,25 @@
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
+import GoBack from "./GoBack";
+import Alert from "./Alert";
+import { useState, useContext } from "react";
+import Context from "../context/Context";
+
+const Layout = ({ children }) => {
+  const [showSidebar, setShowSidebar] = useState(true);
+  const { message } = useContext(Context);
+
+  return (
+    <div className="app">
+      {message ? <Alert msg={message} /> : null}
+      <Sidebar hideSidebar={!showSidebar} />
+      <Topbar toggleSidebar={() => setShowSidebar((prev) => !prev)} sideBarIsClosed={!showSidebar} />
+      <div className="page" style={showSidebar ? { marginLeft: "17.5%" } : null}>
+        <GoBack />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
