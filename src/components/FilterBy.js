5fbe4fd5b7b51617333794eddbd2900f4dd54249
therealrinku <backupfb2032@gmail.com>
2021-04-10 08:54:29 +0545

    filter by styles created

--- src/components/FilterBy.js
+++ src/components/FilterBy.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { BiCaretDown } from "react-icons/all";
+import "../styles/Filterby.css";
 
 const FilterBy = ({ onlySearchBox, setFilterOption }) => {
   const [showFilterOptions, setShowFilterOptions] = useState(false);
   return (
-    <div style={onlySearchBox ? { display: "none" } : null}>
-      <button type="button" onClick={() => setShowFilterOptions((prev) => !prev)}>
+    <div style={onlySearchBox ? { display: "none" } : null} className="filterby">
+      <button onClick={() => setShowFilterOptions((prev) => !prev)}>
         <BiCaretDown />
         <span>Search by</span>
       </button>
