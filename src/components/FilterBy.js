3afa4729e91c05b9382351a38727be33756d5188
therealrinku <backupfb2032@gmail.com>
2021-04-10 08:51:02 +0545

    filter by created

---
+++ src/components/FilterBy.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { BiCaretDown } from "react-icons/all";
+
+const FilterBy = ({ onlySearchBox, setFilterOption }) => {
+  const [showFilterOptions, setShowFilterOptions] = useState(false);
+  return (
+    <div style={onlySearchBox ? { display: "none" } : null}>
+      <button type="button" onClick={() => setShowFilterOptions((prev) => !prev)}>
+        <BiCaretDown />
+        <span>Search by</span>
+      </button>
+
+      <section className="options" style={!showFilterOptions ? { display: "none" } : null}>
+        <button onClick={() => setFilterOption("Class")}>Class</button>
+        <button onClick={() => setFilterOption("Name")}>Name</button>
+        <button onClick={() => setFilterOption("RollNo")}> Rollno</button>
+      </section>
+    </div>
+  );
+};
+
+export default FilterBy;
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,7 +3,6 @@ import { BiCaretDown, HiOutlineSearch } from "react-icons/all";
 import "../styles/Filters.css";
 
 const Filters = ({ searchQuery, setSearchQuery, onlySearchBox }) => {
-  const [showFilterOptions, setShowFilterOptions] = useState(false);
   const [showSortOptions, setShowSortOptions] = useState(false);
 
   return (
@@ -17,22 +16,6 @@ const Filters = ({ searchQuery, setSearchQuery, onlySearchBox }) => {
           onChange={(e) => setSearchQuery(e.target.value)}
         />
       </form>
-
-      <div style={onlySearchBox ? { display: "none" } : null}>
-        <button onClick={() => setShowFilterOptions((prev) => !prev)}>
-          <BiCaretDown />
-          <span>Filter by</span>
-        </button>
-
-        <section className="options" style={!showFilterOptions ? { display: "none" } : null}>
-          <button>Class 8</button>
-          <button>Class 7</button>
-          <button>Class 6</button>
-          <button>Class 5</button>
-          <button>Class 4</button>
-          <button>Class 3</button>
-        </section>
-      </div>
 
       <div style={onlySearchBox ? { display: "none" } : null}>
         <button onClick={() => setShowSortOptions((prev) => !prev)}>
